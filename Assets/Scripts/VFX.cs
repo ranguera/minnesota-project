a6e5d8008659d34970bfb937d76fae1b1d1b6c37
Roger Anguera <ranguera@gmail.com>
2019-06-02 23:30:23 -0700

    cool looking effects. no tracking or attractor

---
+++ Assets/Scripts/VFX.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class VFX : MonoBehaviour
+{
+    public VisualEffect vfx;
+    
+    public Gradient gradient_good = null;
+    public Gradient gradient_sick = null;
+
+    public float transition_frames = 250;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1)) // sick to good
+        {
+            StartCoroutine(Vector3Transition(Vector3.one * .4f, Vector3.one * .2f, "Size"));
+            StartCoroutine(FloatTransition(1.2f, 1.05f, "Radius"));
+            StartCoroutine(FloatTransition(2f, 1f, "Intensity"));
+            StartCoroutine(FloatTransition(.4f, .2f, "PartSize"));
+
+            StartCoroutine(GradientTransition(false));
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) // good to sick
+        {
+            StartCoroutine(Vector3Transition(Vector3.one * .2f, Vector3.one * .4f, "Size"));
+            StartCoroutine(FloatTransition(1.05f, 1.2f, "Radius"));
+            StartCoroutine(FloatTransition(1f, 2f, "Intensity"));
+            StartCoroutine(FloatTransition(.2f, .4f, "PartSize"));
+
+            StartCoroutine(GradientTransition(true));
+        }
+    }
+
+    private IEnumerator FloatTransition(float from, float to, string f)
+    {
+        for (int i = 0; i < transition_frames; i++)
+        {
+            vfx.SetFloat(f, Mathf.Lerp(from, to, i/ transition_frames));
+            yield return null;
+        }
+    }
+
+    private IEnumerator Vector3Transition(Vector3 from, Vector3 to, string v)
+    {
+        for (int i = 0; i < transition_frames; i++)
+        {
+            vfx.SetVector3(v, Vector3.Lerp(from, to, i/ transition_frames));
+            yield return null;
+        }
+    }
+
+    private IEnumerator GradientTransition(bool good_to_sick)
+    {
+        Gradient temp = new Gradient();
+        temp.colorKeys = new GradientColorKey[4];
+        temp.alphaKeys = new GradientAlphaKey[4];
+
+        GradientColorKey[] tempck = new GradientColorKey[4];
+
+        Gradient from = null;
+        Gradient to = null;
+
+        from = good_to_sick ? gradient_good : gradient_sick;
+        to = good_to_sick ? gradient_sick : gradient_good;
+
+        temp.alphaKeys = gradient_sick.alphaKeys;
+
+        // Create a gradient for each of the color keys to blend the two gradients
+        GradientColorKey[] colorKey1 = new GradientColorKey[2];
+        colorKey1[0].color = from.colorKeys[0].color;
+        colorKey1[0].time = 0.0f;
+        colorKey1[1].color = to.colorKeys[0].color;
+        colorKey1[1].time = 1f;
+
+        GradientColorKey[] colorKey2 = new GradientColorKey[2];
+        colorKey2[0].color = from.colorKeys[1].color;
+        colorKey2[0].time = 0.0f;
+        colorKey2[1].color = to.colorKeys[1].color;
+        colorKey2[1].time = 1f;
+
+        GradientColorKey[] colorKey3 = new GradientColorKey[2];
+        colorKey3[0].color = from.colorKeys[2].color;
+        colorKey3[0].time = 0.0f;
+        colorKey3[1].color = to.colorKeys[2].color;
+        colorKey3[1].time = 1f;
+
+        GradientColorKey[] colorKey4 = new GradientColorKey[2];
+        colorKey4[0].color = from.colorKeys[3].color;
+        colorKey4[0].time = 0.0f;
+        colorKey4[1].color = to.colorKeys[3].color;
+        colorKey4[1].time = 1f;
+
+        Gradient g1 = new Gradient();
+        Gradient g2 = new Gradient();
+        Gradient g3 = new Gradient();
+        Gradient g4 = new Gradient();
+
+        g1.SetKeys(colorKey1, to.alphaKeys);
+        g2.SetKeys(colorKey2, to.alphaKeys);
+        g3.SetKeys(colorKey3, to.alphaKeys);
+        g4.SetKeys(colorKey4, to.alphaKeys);
+
+        // Evaluate each of the colorkey gradients, create a new temp gradient and assign to vfx
+        for (int i = 0; i < transition_frames; i++)
+        {
+            tempck[0].color = g1.Evaluate(i / transition_frames);
+            tempck[0].time = to.colorKeys[0].time;
+            tempck[1].color = g2.Evaluate(i / transition_frames);
+            tempck[1].time = to.colorKeys[1].time;
+            tempck[2].color = g3.Evaluate(i / transition_frames);
+            tempck[2].time = to.colorKeys[2].time;
+            tempck[3].color = g4.Evaluate(i / transition_frames);
+            tempck[3].time = to.colorKeys[3].time;
+
+            temp.SetKeys(tempck, temp.alphaKeys);
+            
+            vfx.SetGradient("Gradient", temp);
+
+            yield return null;
+        }
+    }
+}
